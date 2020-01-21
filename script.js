d2f58b5f6e93745c08bff6b1f1bcf34d9dd337b6
Dandarr <55989660+Dandarr@users.noreply.github.com>
2020-01-21 15:15:19 +0000

    Setting up initialfile structure

---
+++ script.js
@@ -0,0 +1,21 @@
+function svLcl() {
+  window.print();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init(){
+  window.printBtn.addEventListener('click', svLcl);
+  window.fveBtn.addEventListener('click', fave);
+}
