6875848d7042ffdd32649e0e4114b44545ed2c51
Dandarr <55989660+Dandarr@users.noreply.github.com>
2020-01-21 15:28:01 +0000

    Planning and basic funtionalities pt 1

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ function svLcl() {
   window.print();
 }
 
+function fave(){
+currentvalue = document.getElementById('fvOnOff').value;
+  if(currentvalue == "Off"){
+      document.getElementById("fvOnOff").value="On";
+      //make image element source = gold star
+      //document . whatever = checked
+  }
+  else{
+      document.getElementById("itaonoff").value="Off";
+      //make image element source = unstarred
+  }
+}
 
-
-
-
-
-
-
+//if checked state = checked then add RandUserId to database of people who have faved item.
 
 
 
@@ -16,6 +23,6 @@ function svLcl() {
 
 
 function init(){
-  window.printBtn.addEventListener('click', svLcl);
+  window.Download.addEventListener('click', svLcl);
   window.fveBtn.addEventListener('click', fave);
 }
