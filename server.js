a7089978d50330061a61e1fbfd0bad8f9c33cdbd
UP885188 <55978223+UP885188@users.noreply.github.com>
2020-03-13 17:17:57 +0000

    Connect to database.

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-function test(ic, le) {
-    return
-}
+'use strict';
+
+const Connection = require('tedious').Connection;
+const config = {
+  server: 'up885188.myvm.port.ac.uk',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: 'up885188',
+      password: 'recipeapp'
+    }
+  },
+  options: {
+    encrypt: true,
+    database: 'recipeapp'
+  }
+};
+const connection = new Connection(config);
+connection.on('connect', function(err) {
+  console.log("Connected");
+})
