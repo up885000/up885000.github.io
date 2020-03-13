7251f5da8ffa4de80ffabd27427763612c9b6ffb
Austin <zzirnathan@hotmail.com>
2020-03-13 17:15:43 +0000

    updated inserdata.sql
    reworked some statements and converted to work in PSQL.

--- insertdata.sql
+++ insertdata.sql
@@ -1,68 +1,68 @@
 /* This file contains all the SQL code to insert data into the server */
 
 /* SQL for the category table */
-INSERT INTO category (category_id, category_name) VALUES (1 , "African");
-INSERT INTO category (category_id, category_name) VALUES (2 , "Rice");
-INSERT INTO category (category_id, category_name) VALUES (3 , "Chinese");
-INSERT INTO category (category_id, category_name) VALUES (4 , "Seafood");
-INSERT INTO category (category_id, category_name) VALUES (5 , "Side");
-INSERT INTO category (category_id, category_name) VALUES (6 , "British");
-INSERT INTO category (category_id, category_name) VALUES (7 , "Dessert");
-INSERT INTO category (category_id, category_name) VALUES (8 , "Vegetarian");
-INSERT INTO category (category_id, category_name) VALUES (9 , "Chicken");
-INSERT INTO category (category_id, category_name) VALUES (10 , "Mexican");
+INSERT INTO category (category_id, category_name) VALUES (1 , 'African');
+INSERT INTO category (category_id, category_name) VALUES (2 , 'Rice');
+INSERT INTO category (category_id, category_name) VALUES (3 , 'Chinese');
+INSERT INTO category (category_id, category_name) VALUES (4 , 'Seafood');
+INSERT INTO category (category_id, category_name) VALUES (5 , 'Side');
+INSERT INTO category (category_id, category_name) VALUES (6 , 'British');
+INSERT INTO category (category_id, category_name) VALUES (7 , 'Dessert');
+INSERT INTO category (category_id, category_name) VALUES (8 , 'Vegetarian');
+INSERT INTO category (category_id, category_name) VALUES (9 , 'Chicken');
+INSERT INTO category (category_id, category_name) VALUES (10 , 'Mexican');
 
 /* SQL for the ingredients table */
 
 /* Fish and Chips */
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (1 , "Sunflower Oil");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (2 , "Cod Fillets");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (3 , "Flour");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (4 , "Black Pepper");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (5 , "Lager");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (6 , "Potato");
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (1 , 'Sunflower Oil');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (2 , 'Cod Fillets');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (3 , 'Flour');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (4 , 'Black Pepper');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (5 , 'Lager');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (6 , 'Potato');
 
 /* Battenburg Cake */
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (7 , "Butter");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (8 , "Sugar");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (9 , "Eggs");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (10 , "Vanilla Extract");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (11 , "Icing Sugar");
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (7 , 'Butter');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (8 , 'Sugar');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (9 , 'Eggs');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (10 , 'Vanilla Extract');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (11 , 'Icing Sugar');
 
 /* Jam roly-poly */
 
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (12 , "Milk");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (13 , "Strawberry Jam");
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (12 , 'Milk');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (13 , 'Strawberry Jam');
 /* Also: Flour, Sugar */
 
 /* Chicken Burritos */
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (14 , "Chicken Thighs");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (15 , "Mayonnaise");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (16 , "Sour Cream");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (17 , "Lime");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (18 , "Tomato");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (19 , "Rice");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (20 , "Tortilla");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (21 , "Mozarella");
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (14 , 'Chicken Thighs');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (15 , 'Mayonnaise');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (16 , 'Sour Cream');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (17 , 'Lime');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (18 , 'Tomato');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (19 , 'Rice');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (20 , 'Tortilla');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (21 , 'Mozarella');
 
 /* Firecracker prawns with stir-fried greens */
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (22 , "Spring Oninons");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (23 , "Chillies");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (25 , "Oyster Sauce");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (26 , "Vinegar");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (27 , "Soy Sauce");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (22 , "Prawns");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (23 , "Olive Oil");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (24 , "Park Choi");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (25 , "Sesame Oil");
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (22 , 'Spring Oninons');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (23 , 'Chillies');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (25 , 'Oyster Sauce');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (26 , 'Vinegar');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (27 , 'Soy Sauce');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (28 , 'Prawns');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (29 , 'Olive Oil');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (30 , 'Park Choi');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (31 , 'Sesame Oil');
 
 
 /* Jollof Rice */
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (22 , "Passata");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (23 , "Puree");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (24 , "Scotch Bonnet Chillies");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (25 , "Onions");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (26 , "Vegetable Oil");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (27 , "Tomatoes");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (22 , "Chicken Stock");
-INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (24 , "Plantains");
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (32 , 'Passata');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (33 , 'Puree');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (34 , 'Scotch Bonnet Chillies');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (35 , 'Onions');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (36 , 'Vegetable Oil');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (37 , 'Tomatoes');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (38 , 'Chicken Stock');
+INSERT INTO ingredient (ingredient_id, ingredient_name) VALUES (39 , 'Plantains');
