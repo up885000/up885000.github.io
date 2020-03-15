69da9f1c02e082173640397c8cf382f6e4fe2c47
Dandarr <thomas.smith.6007@gmail.com>
2020-03-15 15:08:06 +0000

    added needed media and made some fixes to html

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-function test(ic, le) {
-    return
-}
-// look up the sql query select 
+
+// 'use strict';
+
+// const {Client} = require('pg');
+// const client = new Client({
+//     user: "up885000",
+//     password: "softwareServer",
+//     host: "up885000@up885000.myvm.port.ac.uk",
+//     port: 5432,
+//     database: "recipeapp"
+// });
+
+// client.connect();
+// //.then(() => console.log("Connected successfuly"));
+
