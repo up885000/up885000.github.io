bf2931af686f5185158dacc4c82d2b462d48278c
Austin <zzirnathan@hotmail.com>
2020-03-20 19:16:18 +0000

    updated reviewinsert.sql
    added insert data for review table.

--- database/sql_insert_data/reviewinsert.sql
+++ database/sql_insert_data/reviewinsert.sql
@@ -1,14 +1,14 @@
-/* This file contains all the SQL code to insert data for the review table into the server */
+/* This file contains all the SQL code to insert data for the reviews table into the server */
 
-INSERT INTO review (review_id, recipe_id, rating, review) VALUES (1 , 1, 4, 'This recipe was very good. I made this for 8, and we all thoroughly enjoyed it.');
+INSERT INTO reviews (review_id, recipe_id, rating, review) VALUES (1 , 1, 4, 'This recipe was very good. I made this for 8, and we all thoroughly enjoyed it.');
 
-INSERT INTO review (review_id, recipe_id, rating, review) VALUES (2 , 2, 5, 'Quick and easy to cook. A well deserved 5-star side dish.');
+INSERT INTO reviews (review_id, recipe_id, rating, review) VALUES (2 , 2, 5, 'Quick and easy to cook. A well deserved 5-star side dish.');
 
-INSERT INTO review (review_id, recipe_id, rating, review) VALUES (3 , 3, 4, 'I made this last night. Everything turned out delicious! Fries were yummy and the fish was tender and it was a great dinner.');
+INSERT INTO reviews (review_id, recipe_id, rating, review) VALUES (3 , 3, 4, 'I made this last night. Everything turned out delicious! Fries were yummy and the fish was tender and it was a great dinner.');
 
-INSERT INTO review (review_id, recipe_id, rating, review) VALUES (4 , 4, 5, 'GREAT recipe - easy to make. very authentic - however, you must spread the apricot jam on the marzipan with a pastry brush before you wrap the cake in it - this helps it to stick to the cake and it also tastes delicious! Try it with raspberry jam too for an alternative. Also, the cake mix looks too stiff even with a little milk - but you need the cake to be firm and hold it''s shape - so
+INSERT INTO reviews (review_id, recipe_id, rating, review) VALUES (4 , 4, 5, 'GREAT recipe - easy to make. very authentic - however, you must spread the apricot jam on the marzipan with a pastry brush before you wrap the cake in it - this helps it to stick to the cake and it also tastes delicious! Try it with raspberry jam too for an alternative. Also, the cake mix looks too stiff even with a little milk - but you need the cake to be firm and hold it''s shape - so
 don''t be fooled!!! The recipe is spot on.Excellent ******');
 
-INSERT INTO review (review_id, recipe_id, rating, review) VALUES (5 , 5, 1, 'Followed this recipe rather than mary berrys which said to use self raising flour. Wished I''d have stuck with Mary berry, this was so terrible I put it out for the birds.');
+INSERT INTO reviews (review_id, recipe_id, rating, review) VALUES (5 , 5, 1, 'Followed this recipe rather than mary berrys which said to use self raising flour. Wished I''d have stuck with Mary berry, this was so terrible I put it out for the birds.');
 
-INSERT INTO review (review_id, recipe_id, rating, review) VALUES (6 , 6, 5, 'Fun recipe who doesn’t love a good chicken burrito? And better when is homemade and made with lots of love');
+INSERT INTO reviews (review_id, recipe_id, rating, review) VALUES (6 , 6, 5, 'Fun recipe who doesn’t love a good chicken burrito? And better when is homemade and made with lots of love');
