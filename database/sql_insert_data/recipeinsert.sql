2267d791290b0a5fb7edc40eb809eb6f99ec4bf9
Austin <zzirnathan@hotmail.com>
2020-03-20 14:39:58 +0000

    updated recipeinsert.sql
    prepared recipeinsert.sql for data entry.

---
+++ database/sql_insert_data/recipeinsert.sql
@@ -0,0 +1,23 @@
+/* This file contains all the SQL code to insert data for the recipe table into the server */
+
+/* Jollof Rice */
+
+
+
+/* Firecracker prawns with stir-fried greens */
+
+
+
+/* Fish and Chips */
+
+
+
+/* Battenberg cake */
+
+
+
+/* Jam roly-poly */
+
+
+
+/* Chicken burritos */
