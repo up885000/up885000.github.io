541b1bb04e30d6807b913174ceef27761735f54c
Austin <zzirnathan@hotmail.com>
2020-03-20 19:26:35 +0000

    added recipe_ingredientsinsert.sql
    Added insert statements for recipes 4, 5, and 6.

---
+++ database/sql_insert_data/recipe_ingredientsinsert.sql
@@ -0,0 +1,93 @@
+/* This file contains all the SQL code to insert data for the recipe_ingredients table into the server */
+
+/*/ Jollof Rice */
+
+
+
+/* Firecracker prawns with stir-fried greens */
+
+
+
+/* Fish and chips */
+
+
+
+/* Battenburg Cake */
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4, 7, 5, 175, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4, 7, 7, 6, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 8, 5, 175, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 8, 7, 6, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 9, 1, 3, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 3, 5, 175, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 3, 7, 6, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 10, 4, 1, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (4 , 11, 4, 3, 'neutral');
+
+/* Jam roly-poly */
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (5 , 3, 5, 200, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (5 , 3, 7, 7, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (5 , 8, 4, 1, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (5 , 12, 2, 150, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (5 , 12, 3, 5, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (5 , 13, 4, 6, 'neutral');
+
+/* Chicken burritos */
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 14, 5, 800, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 14, 7, 28, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 15, 4, 2, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 16, 4, 2, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 17, 1, 1, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 37, 1, 2, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 19, 5, 150, 'metric');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 19, 7, 5, 'imperial');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 20, 1, 4, 'neutral');
+
+INSERT INTO recipe_ingredients (recipe_id , ingredients_id, measurement_id, quantity, type) 
+VALUES (6 , 21, 4, 4, 'neutral');
