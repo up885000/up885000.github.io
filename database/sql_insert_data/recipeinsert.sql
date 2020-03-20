ffc6c7c049d3d1f2c18ffb6d7a55993003145b8f
Austin <zzirnathan@hotmail.com>
2020-03-20 15:32:59 +0000

    updated recipeinsert.sql
    Completed insert statements for recipes 4, 5, and 6.
    
    Also added folder to contain the images for these recipes.

--- database/sql_insert_data/recipeinsert.sql
+++ database/sql_insert_data/recipeinsert.sql
@@ -2,22 +2,116 @@
 
 /* Jollof Rice */
 
-
+INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
+, cooking_time, difficulty, recipe_serving, image_location) 
+VALUES (1 , 'recipe name', 'description', preptime, cooktime, 'difficulty', recipeserv, '/database/images/jollof_rice.jpg');
 
 /* Firecracker prawns with stir-fried greens */
 
-
+INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
+, cooking_time, difficulty, recipe_serving, image_location) 
+VALUES (2 , 'recipe name', 'description', preptime, cooktime, 'difficulty', recipeserv, 'imagelocation');
 
 /* Fish and Chips */
 
-
+INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
+, cooking_time, difficulty, recipe_serving, image_location) 
+VALUES (3 , 'recipe name', 'description', preptime, cooktime, 'difficulty', recipeserv, 'imagelocation');
 
 /* Battenberg cake */
 
+INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
+, cooking_time, difficulty, recipe_serving, image_location) 
+VALUES (4 , 'Battenberg cake'
+, 'Battenberg cake is a true classic for afternoon tea. The Hairy Bikers recipe makes it foolproof.
 
+Method
+
+1. Preheat the oven to 190C/375F/Gas 5.
+
+2. Grease a 20cm/8in square, loose-based cake tin with butter.
+
+3. Take a 30cm x 20cm/12in x 8in strip of baking parchment and make a 8cm/3infold in the centre. This will create a division in the cake so that the two
+differently coloured sponges can be cooked at the same time.
+
+4. Line the tin with the baking parchment, keeping the division in the centre
+
+5. Put the butter, sugar, eggs, flour and vanilla in a food processor and pulse until well combined.
+
+6. Transfer the batter to a bowl set on scales, remove half of the batter and put it in a different bowl.
+
+7. Add a small dab of red food colouring to one bowl and fold it into the batter until it is well blended.
+
+8. Spoon the cake batters into each side of the prepared tin and smooth the surface with the back of a spoon.
+
+9. Bake in the centre of the oven for about 25 minutes, or until the sponges have risen. Cool in the tin for five minutes, then slide a knife around the outside of each sponge and turn them out onto a wire rack. If the sponges have risen unevenly, press the surface gently until level. Leave until completely cold
+
+10. To assemble the cake, first place one sponge on top of the other and trim off the crusty edges so they are both the same size. Cut the sponges in half lengthways to make four long rectangles.
+
+11. Warm the apricot jam in a saucepan then press through a fine sieve.
+
+12. Brush the long side of one of the sponges with jam and sandwich together with a sponge of a contrasting colour. Do the same with the other two sponges.
+
+13. Sandwich the two pairs of sponges together like a checker board and brush the top and sides with jam.
+
+14. Place the marzipan on a surface dusted with icing sugar and roll into a rectangle of about 40cm x 20cm/16in x10in; it should be large enough to wrap the cake completely, leaving the ends exposed, and be about 5mm/¼in thick.
+
+15. Turn the cake upside down on the marzipan and brush the underside of the sponges with jam.
+
+16. Wrap the marzipan around the cake, pressing it gently onto the surface of the sponges, and press the edges together to make a firm join.
+
+17. Turn back over with the seam underneath, trim a thin slice off each end and place on a serving plate.'
+
+, '00:45', '00:20', 'Hard', 1, '/database/images/battenburg_cake.jpg');
 
 /* Jam roly-poly */
 
+INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
+, cooking_time, difficulty, recipe_serving, image_location) 
+VALUES (5 , 'Jam roly-poly'
+, 'Try a recipe based on the Hairy Bikers recipe for jam roly-poly. Its an easy homemade version of a childhood classic. Serve with plenty of custard.
 
+Method
+
+1. Preheat the oven to 200C/180C Fan/Gas6. Butter a large sheet of baking paper and set aside.
+
+2. Stir the flour, suet, sugar and salt in a large bowl until fully combined. Slowly stir in the milk to form a soft, spongy dough.
+
+3. Tip the dough out onto a floured surface and knead for a few minutes. Roll the dough out to a 22cm x32cm/8½in x 13in rectangle.
+
+4. Spread the jam onto the dough, leaving a 1.5cm/½in border. Gently roll the dough up from the short end and transfer to the greaseproof paper, seam-side down. Wrap the roly poly in the baking paper, making a long pleatin the paper to allow the pudding to expand as it cooks. Twist the ends ofthe paper like a Christmas cracker and tie tightly with kitchen string, to seal the pudding inside. Repeat the process with a large piece of kitchen foil.
+
+5. Place the pudding onto a roasting rack set on a deep-sided roasting tin. Pour boiling water halfway up the roasting tin and cook in the oven for 30–35 minutes.
+
+6. Remove the pudding from the oven, unwrap the kitchen foil, then snip the string and unwrap the paper.
+
+7. The pudding should be well risen and lightly browned in places. Don’t worry if the jam has made its way through to the outside of the pudding a little – it will taste all the more delicious.
+
+8. Put on a board or serving plate and cut into thick slices. Serve with lots of hot custard or cream.'
+
+, '00:30', '00:45', 'Medium', 6, '/database/images/jam_roly_poly.jpg');
 
 /* Chicken burritos */
+
+INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
+, cooking_time, difficulty, recipe_serving, image_location) 
+VALUES (6 , 'Chicken burritos'
+, 'These authentic chicken burritos are the real deal. Topped with pico de gallo, chipotle crema, guacamole, soured cream and cheese, guests won''t be disappointed.
+
+Method
+
+1. To make the chicken, mix the lime juice, garlic, oil, oregano, chilli, sugar and salt and pepper together in a large bowl. Add the chicken, cover and place in the fridge for at least 2 hours.
+
+2. Meanwhile, to make the chipotle crema, mix the chipotles en adobo, soured cream and mayonnaise with a squeeze of lemon juice and a pinch of salt and set aside.
+
+3. To make the pico de gallo, mix all the ingredients together with ½ teaspoon of salt in a bowl and set aside.
+
+4. To make the guacamole, pound the chilli in a pestle and mortar with the onionand ¼ teaspoon of salt, into a lumpy paste. Add the avocado and break up the flesh roughly with a fork. The result should be lumpy not smooth. Stir in the lime juice, to taste, and the chopped coriander.
+
+5. Place a frying pan over a high heat and cook the chicken thighs, without the marinade, until browned and sealed. Add the marinade to the pan, cover with alid and cook for 10–15 minutes, or until the chicken is cooked through as the juices run clear. Slice the chicken into strips.
+
+6. Meanwhile, cook the rice according to packet instructions.
+
+7. Divide the tortillas between four plates and top with the rice, chicken, sauces, mozzarella and lettuce. Fold up the bottom of each tortilla, then fold the sides in and roll to contain the filling. Cut in half to serve.'
+
+, '01:30', '00:20', 'Medium', 4, '/database/images/chicken_burrito.jpg');
