d2d7da0b5c1da59cbb79c0dd9a2110524332153b
UP885188 <55978223+UP885188@users.noreply.github.com>
2020-03-21 14:41:53 +0000

    express server

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 'use strict';
 
-const http = require('http');
-const fs = require('fs');
-
-const PORT=1234;
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;
-
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(PORT);
-});
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public')); //__dir and not _dir
+var port = 1234; // you can use any port
+app.listen(port);
+console.log('server on' + port);
