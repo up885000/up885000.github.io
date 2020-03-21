d9d86e4349fe338fbcf82688533e8de3aa712e28
Ben <up885000@myport.ac.uk>
2020-03-21 23:39:06 +0000

    server psql connect

--- server.js
+++ server.js
@@ -25,3 +25,40 @@ app.use(express.static(`${__dirname }/webpages`));
 server.listen(port, () => {
     console.log('Server started:', `http://${ip.address()}:${port}`)
 });
+
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'up885000',
+        password: 'softwareServer',
+        server: 'localhost', 
+        database: 'recipeapp' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from ingredients', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5432, function () {
+    console.log('Server is running..');
+});
