e9f884399e1923302421dfb60b4ff785ff5361ec
Austin <zzirnathan@hotmail.com>
2020-03-21 13:21:24 +0000

    updated insert sql files
    Some files had incorrect table names which was preventing inserts.

--- database/sql_insert_data/measurementsinsert.sql
+++ database/sql_insert_data/measurementsinsert.sql
@@ -1,10 +1,10 @@
 /* This file contains all the SQL code to insert data for the measurements table into the server */
 
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (1 , ' ');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (2 , 'ml');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (3 , 'fl oz');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (4 , 'tbsp');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (5 , 'g');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (6 , 'lb');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (7 , 'oz');
-INSERT INTO measurement (measurement_id , measurement_name) VALUES (8 , 'tsp');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (1 , ' ');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (2 , 'ml');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (3 , 'fl oz');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (4 , 'tbsp');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (5 , 'g');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (6 , 'lb');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (7 , 'oz');
+INSERT INTO measurements (measurement_id , measurement_name) VALUES (8 , 'tsp');
