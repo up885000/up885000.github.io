9c6969ea0df1e6ee98c685638386e06588dd9f9e
Ben <up885000@myport.ac.uk>
2020-03-21 19:30:56 +0000

    script fix pt 4

--- webpages/script.js
+++ webpages/script.js
@@ -15,9 +15,9 @@ let currentvalue = false;
 
 function fave() {
   let x = document.getElementById('fave');
-  if (x.src == "offstar.png") {
+  if (x.src == "media/offstar.png") {
       currentvalue = false;
-  } else if (x.src == "onstar.png") {
+  } else if (x.src == "media/onstar.png") {
       currentvalue = true;
   }
   //currentvalue = document.getElementById('fvOnOff').value;
@@ -25,7 +25,7 @@ function fave() {
       currentvalue = true;
       //document.getElementById("fvOnOff").valu = "True";
       // switches the image source from faved to unfaved
-      document.getElementById('fave').src = 'onStar.png';
+      document.getElementById('fave').src = 'media/onStar.png';
       //update sql database that user has faved a recipe
   }
   //Add local storage element here **
@@ -33,7 +33,7 @@ function fave() {
       currentvalue = false;
       //document.getElementById("fvOnoff").value="False";
       // we need to edit the fave stars to be the same size and file format
-      document.getElementById('fave').src = 'offStar.png';
+      document.getElementById('fave').src = 'media/offStar.png';
       //update sql database that user has unfaved recipe
   }
 }
