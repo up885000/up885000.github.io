e549e05a321bf516ed21d66dbac81d146dde019d
Shubas <up813077@myport.ac.uk>
2020-03-23 21:39:15 +0000

    recipeinsert.sql
    finished the remaining 3 recipes and added and embedded the images as well

--- database/sql_insert_data/recipeinsert.sql
+++ database/sql_insert_data/recipeinsert.sql
@@ -3,25 +3,71 @@
 /* Jollof Rice */
 
 INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
-, cooking_time, difficulty, recipe_serving, image_location) 
-VALUES (1 , 'recipe name', 'description', preptime, cooktime, 'difficulty', recipeserv, '/database/images/jollof_rice.jpg');
+, cooking_time, difficulty, recipe_serving, image_location)
+VALUES (1 , 'Jollof Rice', 'Jollof rice is a spicy African rice dish that cooks in one pan, uses lots of veggies.
+
+Method
+
+1. Put the passata, tomato purée, chillies, onions, peppers, garlic, and paprika in a blender or food processor and blend until smooth.
+
+2. Heat the olive oil in a large saucepan over medium heat. Add the cherry tomatoes and the blended tomato sauce. Bring to the boil, then reduce the heat slightly and simmer for 5 minutes.
+
+3. Add the stock, rice, 1 teaspoon salt and a large pinch of black pepper. Stir to combine and bring to the boil. Reduce the heat and simmer for 10–12 minutes.
+
+4. Meanwhile, heat the sunflower oil in a frying pan over medium heat. Fry the plantain for a few minutes on each side until golden and tender.
+
+5. Spoon the jollof rice onto warmed plates and add the plantain on the side. Garnish with the chopped coriander and serve with a green salad alongside.'
+
+,'00:30', '00:45', 'Medium', 6, '/database/images/jollof_rice.jpg');
+
+
 
 /* Firecracker prawns with stir-fried greens */
 
 INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
-, cooking_time, difficulty, recipe_serving, image_location) 
-VALUES (2 , 'recipe name', 'description', preptime, cooktime, 'difficulty', recipeserv, 'imagelocation');
+, cooking_time, difficulty, recipe_serving, image_location)
+VALUES (2 , 'Firecracker Prawns with stir-fride greens', 'Packed with protein, flavour, colour and punch, this low-calorie, quick dinner will become a weekly favourite.
+
+Method
+
+1. Toast the fennel and cumin seeds in a small frying pan for 1–2 minutes, or until fragrant. Crush with a pestle and mortar and tip into a mixing bowl. Add the spring onions, chillies, coriander, lime juice, oyster sauce, star anise, vinegar and soy sauce. Stir in the prawns and marinate for 10 minutes.
+
+2. Preheat the grill to its highest setting.
+
+3. Spread the prawns on a grill rack and grill for 3–4 minutes, or until pink and cooked through. Keep warm.
+
+4. To make the stir-fried greens, heat the oil in a wide, non-stick frying pan over high heat. Add the pak choi, ginger and soy sauce and cook for 4–5 minutes, or until just wilted. Stir in the sesame oil.
+
+5. Serve the greens with the prawns immediately.'
+
+, '00:30', '00:30', 'Easy', 2, '/database/images/firecracker_prawns.jpg');
 
 /* Fish and Chips */
 
 INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
-, cooking_time, difficulty, recipe_serving, image_location) 
-VALUES (3 , 'recipe name', 'description', preptime, cooktime, 'difficulty', recipeserv, 'imagelocation');
+, cooking_time, difficulty, recipe_serving, image_location)
+VALUES (3 , 'Fish and Chips', 'You can not beat homemade fish & chips, get perfect results with Jo Pratts easy fish supper - a classic British favourite
+
+Method
+
+1. Preheat the oven to 150C/300F/Gas 2 and preheat the dripping or oil to 120C/250F.
+
+2. For the chips, peel the potatoes and cut into whatever size you prefer. Wash well in cold water, drain and pat dry with a clean tea towel. Put the potatoes into the fryer and allow them to fry gently for about 8-10 minutes until they are soft but still pale. Check they are cooked by piercing with a small, sharp knife. Lift out of the pan and leave to cool slightly on greaseproof paper.
+
+3. Increase the heat of the fryer to 180C/350F.
+
+4. Season the fish and dust lightly with flour; this enables the batter to stick to the fish.
+
+5. To make the batter, sift the flour and a pinch of salt into a large bowl and whisk in the lager to give a thick batter, adding a little extra beer if it seems over-thick. It should be the consistency of very thick double cream and should coat the back of a wooden spoon. Season with salt and thickly coat 2 of the fillets with the batter. Carefully place in the hot fat and cook for 8-10 minutes until golden and crispy. Remove from the pan, drain and sit on a baking sheet lined with greaseproof paper, then keep warm in the oven while you cook the remaining 2 fillets in the same way.
+
+6.Once the fish is cooked, return the chips to the fryer and cook for 2-3 minutes or until golden and crispy. Shake off any excess fat and season with salt before serving with the crispy fish. If liked, you can serve with tinned mushy peas and bread and butter, for the authentic experience!'
+
+, '00:25', '00:30', 'Hard', 4, '/database/images/fish_chips.jpg');
 
 /* Battenberg cake */
 
 INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
-, cooking_time, difficulty, recipe_serving, image_location) 
+, cooking_time, difficulty, recipe_serving, image_location)
 VALUES (4 , 'Battenberg cake'
 , 'Battenberg cake is a true classic for afternoon tea. The Hairy Bikers recipe makes it foolproof.
 
@@ -67,7 +113,7 @@ differently coloured sponges can be cooked at the same time.
 /* Jam roly-poly */
 
 INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
-, cooking_time, difficulty, recipe_serving, image_location) 
+, cooking_time, difficulty, recipe_serving, image_location)
 VALUES (5 , 'Jam roly-poly'
 , 'Try a recipe based on the Hairy Bikers recipe for jam roly-poly. Its an easy homemade version of a childhood classic. Serve with plenty of custard.
 
@@ -94,7 +140,7 @@ Method
 /* Chicken burritos */
 
 INSERT INTO recipe (recipe_id, recipe_name, recipe_description, preparation_time
-, cooking_time, difficulty, recipe_serving, image_location) 
+, cooking_time, difficulty, recipe_serving, image_location)
 VALUES (6 , 'Chicken burritos'
 , 'These authentic chicken burritos are the real deal. Topped with pico de gallo, chipotle crema, guacamole, soured cream and cheese, guests won''t be disappointed.
 
