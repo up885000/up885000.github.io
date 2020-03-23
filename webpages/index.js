ff7c14c23d7eb8c2c44c64bf9dfdfdda34e8e380
up885000 <unconfigured@null.spigotmc.org>
2020-03-23 21:57:11 +0000

    review into form

--- webpages/index.js
+++ webpages/index.js
@@ -92,31 +92,28 @@ function revBtn() {
     new_recID.setAttribute('type', 'text');
     new_recID.setAttribute('name', 'recipe_id');
     new_recID.setAttribute('class', 'reviewForm');
-    search.appendChild(new_recID);
+    reviewForm.appendChild(new_recID);
 
     //adds rating for the addReview() function to use
     const new_rating = document.createElement('input');
     new_rating.setAttribute('id', 'rating');
     new_rating.setAttribute('type', 'text');
     new_rating.setAttribute('name', 'rating');
-    new_rating.setAttribute('class', 'reviewForm');
-    search.appendChild(new_rating);
+    reviewForm.appendChild(new_rating);
 
     //adds review for the addReview() function to use
     const new_review = document.createElement('input');
     new_review.setAttribute('id', 'review');
     new_review.setAttribute('type', 'text');
     new_review.setAttribute('name', 'review');
-    new_review.setAttribute('class', 'reviewForm');
-    search.appendChild(new_review);
+    reviewForm.appendChild(new_review);
 
     //adds submit button for the addReview() forms to use
     const new_submit = document.createElement('input');
     new_submit.setAttribute('id', 'addReview');
     new_submit.setAttribute('type', 'submit');
     new_submit.setAttribute('name', 'addReview');
-    new_submit.setAttribute('class', 'reviewForm');
-    search.appendChild(new_submit);
+    reviewForm.appendChild(new_submit);
 
     //gets the function addReview from the server.js file and runs it
     $.getscript("../server.js", addReview(req, res));
