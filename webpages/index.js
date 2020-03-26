47ae6d137d153cd4af17ede938302d24fa2c6241
up885000 <unconfigured@null.spigotmc.org>
2020-03-26 14:01:44 +0000

    enter click

--- webpages/index.js
+++ webpages/index.js
@@ -156,126 +156,140 @@ function revBtn(new_rating, name, new_review, new_submit) {
         reviewForm.appendChild(new_submit);
 
         timesFiredReview = timesFiredReview + 1;
-
         document.getElementById('addReview').addEventListener('click', submitForm);
-    } else {
-        console.log("error you already have a review form in progress");
-    }
-}
 
-/**
- * This function is responsible for submitting a review. It uses the "/addReview" endpoint to do this.
- * 
- */
-async function submitForm() {
-    //gets the function addReview from the server.js file and runs it
-    console.log("initiating recipe review");
-    let recipe_id = document.getElementById('rev').className;
-    let rating = document.getElementById('rating').value;
-    let review = document.getElementById('review').value;
-    let response = fetch('/addReview?recipe_id=' + recipe_id + "&rating=" + rating + "&review=" + review);
-    alert("you have successfully reviewed this recipe");
-    reviewForm.removeChild(document.getElementById('rating'));
-    reviewForm.removeChild(document.getElementById('review'));
-    reviewForm.removeChild(document.getElementById('addReview'));
-    timesFiredReview = timesFiredReview - 1;
-}
+        document.getElementById("review").addEventListener("keyup", function(event) {
+                if (event.keyCode == 13) {
+                    document.getElementById('addReview').click();
+                }
+            }
 
-/**
- * Randomly selects 6 recipes to display when the page loads and then maps these images and associated recipe_id's to the html buttons.
- *  
- */
-async function imageRandomizer() {
-    //query server to add all images into array
-    let response = await fetch('/getRandomImages');
-    let data = await response.json();
-
-    updateButtons(data);
-    selectRecipe(document.getElementById("fI1").className);
-}
-
-/**
- * This function will update the buttons show on screen so that they match the recipes being searched for.
- *
- * @param {JSON[]} data The array of json given by formatting a fetch api request.
- */
-function updateButtons(data) {
-    let cycles = 0;
-    while (cycles < 6) {
-        let currentImage = document.getElementById('fI' + (cycles + 1));
-        if (data[cycles]) {
-            currentImage.src = data[cycles].image_location;
-            currentImage.setAttribute('class', data[cycles].recipe_id);
-            cycles = cycles + 1;
-        } else {
-            currentImage.src = '';
-            currentImage.setAttribute('class', '0');
-            cycles = cycles + 1;
+        }
+        else {
+            console.log("error you already have a review form in progress");
         }
     }
-}
+
+    /**
+     * This function is responsible for submitting a review. It uses the "/addReview" endpoint to do this.
+     * 
+     */
+    async function submitForm() {
+        //gets the function addReview from the server.js file and runs it
+        console.log("initiating recipe review");
+        let recipe_id = document.getElementById('rev').className;
+        let rating = document.getElementById('rating').value;
+        let review = document.getElementById('review').value;
+        let response = fetch('/addReview?recipe_id=' + recipe_id + "&rating=" + rating + "&review=" + review);
+        alert("you have successfully reviewed this recipe");
+        reviewForm.removeChild(document.getElementById('rating'));
+        reviewForm.removeChild(document.getElementById('review'));
+        reviewForm.removeChild(document.getElementById('addReview'));
+        timesFiredReview = timesFiredReview - 1;
+    }
+
+    /**
+     * Randomly selects 6 recipes to display when the page loads and then maps these images and associated recipe_id's to the html buttons.
+     *  
+     */
+    async function imageRandomizer() {
+        //query server to add all images into array
+        let response = await fetch('/getRandomImages');
+        let data = await response.json();
+
+        updateButtons(data);
+        selectRecipe(document.getElementById("fI1").className);
+    }
+
+    /**
+     * This function will update the buttons show on screen so that they match the recipes being searched for.
+     *
+     * @param {JSON[]} data The array of json given by formatting a fetch api request.
+     */
+    function updateButtons(data) {
+        let cycles = 0;
+        while (cycles < 6) {
+            let currentImage = document.getElementById('fI' + (cycles + 1));
+            if (data[cycles]) {
+                currentImage.src = data[cycles].image_location;
+                currentImage.setAttribute('class', data[cycles].recipe_id);
+                cycles = cycles + 1;
+            } else {
+                currentImage.src = '';
+                currentImage.setAttribute('class', '0');
+                cycles = cycles + 1;
+            }
+        }
+    }
 
 
-/**
- * Initialises Javascript buttons, this function is called immideatly when the page loads.
- *
- */
-function init() {
-    imageRandomizer();
-    if (document.getElementById("down")) {
-        document.getElementById("down").addEventListener('click', svLcl);
-    }
-    if (document.getElementById("fave")) {
-        document.getElementById("fave").addEventListener('click', fave);
-    }
-    if (document.getElementById("load")) {
-        document.getElementById("load").addEventListener('click', load);
-    }
-    if (document.getElementById("findRecipe")) {
-        document.getElementById("findRecipe").addEventListener('click', async() => {
-            findRecipe();
+    /**
+     * Initialises Javascript buttons, this function is called immideatly when the page loads.
+     *
+     */
+    function init() {
+        imageRandomizer();
+        if (document.getElementById("down")) {
+            document.getElementById("down").addEventListener('click', svLcl);
+        }
+        if (document.getElementById("fave")) {
+            document.getElementById("fave").addEventListener('click', fave);
+        }
+        if (document.getElementById("load")) {
+            document.getElementById("load").addEventListener('click', load);
+        }
+        if (document.getElementById("findRecipe")) {
+            document.getElementById("findRecipe").addEventListener('click', async() => {
+                findRecipe();
+            });
+        }
+        if (document.getElementById("recipeName")) {
+            document.getElementById("recipeName").addEventListener("keyup", function(event) {
+                if (event.keyCode == 13) {
+                    document.getElementById("findRecipe").click();
+                }
+            })
+        }
+        if (document.getElementById("rev")) {
+            document.getElementById("rev").addEventListener('click', revBtn);
+        }
+        if (document.getElementById("addReview")) {
+            document.getElementById("addReview").addEventListener('click', submitForm);
+        }
+        document.getElementById("showsaves").addEventListener('click', showSaves);
+        document.getElementById("fI1").addEventListener('click', async() => {
+            selectRecipe(document.getElementById("fI1").className);
+        });
+        document.getElementById("fI2").addEventListener('click', async() => {
+            selectRecipe(document.getElementById("fI2").className);
+        });
+        document.getElementById("fI3").addEventListener('click', async() => {
+            selectRecipe(document.getElementById("fI3").className);
+        });
+        document.getElementById("fI4").addEventListener('click', async() => {
+            selectRecipe(document.getElementById("fI4").className);
+        });
+        document.getElementById("fI5").addEventListener('click', async() => {
+            selectRecipe(document.getElementById("fI5").className);
+        });
+        document.getElementById("fI6").addEventListener('click', async() => {
+            selectRecipe(document.getElementById("fI6").className);
         });
     }
-    if (document.getElementById("rev")) {
-        document.getElementById("rev").addEventListener('click', revBtn);
+
+    //add click listeners
+
+    if (typeof window !== 'undefined') {
+        window.addEventListener('load', init);
+        window.addEventListener('featuredRecipe', init);
     }
-    if (document.getElementById("addReview")) {
-        document.getElementById("addReview").addEventListener('click', submitForm);
+
+    //export functions
+
+    if (typeof module !== 'undefined' && module.exports) {
+        exports.revBtn = revBtn;
+        exports.findRecipe = findRecipe;
+        exports.fave = fave;
+        exports.submitForm = submitForm;
+        exports.selectRecipe = selectRecipe;
     }
-    document.getElementById("showsaves").addEventListener('click', showSaves);
-    document.getElementById("fI1").addEventListener('click', async() => {
-        selectRecipe(document.getElementById("fI1").className);
-    });
-    document.getElementById("fI2").addEventListener('click', async() => {
-        selectRecipe(document.getElementById("fI2").className);
-    });
-    document.getElementById("fI3").addEventListener('click', async() => {
-        selectRecipe(document.getElementById("fI3").className);
-    });
-    document.getElementById("fI4").addEventListener('click', async() => {
-        selectRecipe(document.getElementById("fI4").className);
-    });
-    document.getElementById("fI5").addEventListener('click', async() => {
-        selectRecipe(document.getElementById("fI5").className);
-    });
-    document.getElementById("fI6").addEventListener('click', async() => {
-        selectRecipe(document.getElementById("fI6").className);
-    });
-}
-
-//add click listeners
-
-if (typeof window !== 'undefined') {
-    window.addEventListener('load', init);
-    window.addEventListener('featuredRecipe', init);
-}
-
-//export functions
-
-if (typeof module !== 'undefined' && module.exports) {
-    exports.revBtn = revBtn;
-    exports.findRecipe = findRecipe;
-    exports.fave = fave;
-    exports.submitForm = submitForm;
-    exports.selectRecipe = selectRecipe;
-}
