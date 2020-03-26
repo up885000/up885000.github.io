3fe7c720260f4eac0b94cd32aa491eb77f15036f
UP885188 <55978223+UP885188@users.noreply.github.com>
2020-03-26 19:43:04 +0000

    fix for favecheck

--- webpages/index.js
+++ webpages/index.js
@@ -110,7 +110,7 @@ function sortByProperty(property) {
  */
 function favecheck(){
   let fileName = document.getElementById('recipeTitle').innerHTML;
-  if(document.getElementById('fave').src = 'media/offStar.png' && localStorage.getItem(fileName));{
+  if(document.getElementById('fave').src = 'media/offStar.png' && localStorage.getItem(fileName)){
     document.getElementById('fave').src = 'media/onStar.png';
   }
   else if(document.getElementById('fave').src = 'media/onStar.png' && localStorage.getItem(fileName) === null){
